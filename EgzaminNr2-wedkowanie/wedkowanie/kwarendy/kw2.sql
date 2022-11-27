c975a7236523462efcb756df572c6aa0fe069c28
Mateusz Śniechowski <91956404+mateuszSniechowski@users.noreply.github.com>
2022-11-27 17:26:59 +0100

    Delete EgzaminNr2-wedkowanie/wedkowanie directory

--- EgzaminNr2-wedkowanie/wedkowanie/kwarendy/kw2.sql
+++
@@ -1,1 +0,0 @@
-SELECT ryby_id, wymiar_ochronny FROM okres_ochronny WHERE wymiar_ochronny < 30 
--- EgzaminNr2-wedkowanie/wedkowanie/kwarendy/kw3.sql
+++
@@ -1,4 +0,0 @@
-SELECT nazwa, lowisko.akwen, lowisko.wojewodztwo
-FROM ryby
-JOIN lowisko
-ON ryby.id = lowisko.Ryby_id WHERE lowisko.rodzaj = 3
